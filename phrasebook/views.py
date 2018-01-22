a4d97538460bb78edc17b4b4f59d603eafc76f14
JJC <jjc@nlplab.cc>
2018-01-23 00:57:40 +0800

    phrasebook project views

--- phrasebook/views.py
+++ phrasebook/views.py
@@ -1,3 +1,36 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from phrasebook.tpb import get_phrase, get_sentence
+
 
 # Create your views here.
+def phrase_view(request, query):
+    if request.method == 'GET':
+        offset = request.GET.get('offset', 0)
+        query = query.replace("*", "%")
+
+        if not query:
+            return HttpResponse(status=404)
+
+        result = get_phrase(query, offset)
+        return JsonResponse(result)
+    else:
+        return HttpResponse(status=404)
+
+
+def sentence_view(request):
+    if request.method == 'GET':
+        ch = request.GET.get("ch")
+        en = request.GET.get("en")
+
+        if ch is None and en is None:
+            return HttpResponse(status=404)
+
+        result = get_sentence(ch, en)
+        return JsonResponse(result)
+    else:
+        return HttpResponse(status=404)
+
+
+def index(request):
+    return render(request, 'index.html')
